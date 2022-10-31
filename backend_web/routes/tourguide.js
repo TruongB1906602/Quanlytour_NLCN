2714582e071e29a2572354fce0d622681fb34c83
Phuoc Tang <phuocst24@gmail.com>
2022-10-31 16:47:52 +0700

    Fix Bug: api and check condition tourguide

--- backend_web/routes/tourguide.js
+++ backend_web/routes/tourguide.js
@@ -1,35 +1,34 @@
+const Order = require('../models/Order');
 
-const Order = require("../models/Order");
+const User = require('../models/User');
 
-const User = require("../models/User");
+const router = require('express').Router();
 
-const router = require("express").Router();
-
-router.post("/",async (req, res) => {
-  const newTourguide = new Tourguide(req.body);
-  try {
-    const savedTourguide = await newTourguide.save();
-    res.status(200).json(savedTourguide);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-  // return res.send(document);
+router.post('/', async (req, res) => {
+   const newTourguide = new Tourguide(req.body);
+   try {
+      const savedTourguide = await newTourguide.save();
+      res.status(200).json(savedTourguide);
+   } catch (err) {
+      res.status(500).json(err);
+   }
+   // return res.send(document);
 });
 
 //UPDATE
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedTourguide = await Tourguide.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedTourguide);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.put('/:id', async (req, res) => {
+   try {
+      const updatedTourguide = await Tourguide.findByIdAndUpdate(
+         req.params.id,
+         {
+            $set: req.body,
+         },
+         { new: true },
+      );
+      res.status(200).json(updatedTourguide);
+   } catch (err) {
+      res.status(500).json(err);
+   }
 });
 // Chỗ gây lỗi Check Tourguide
 //DELETE
@@ -41,43 +40,28 @@ router.put("/:id", async (req, res) => {
 //     res.status(500).json(err);
 //   }
 // });
-// 
+//
 //GET Tourguide
-router.get("/find/:id", async (req, res) => {
-  try {
-
-    const newOrder = await Order.findById(req.params.id);
-    const newUser = await User.findById(req.params.id);
-    res.status(200).json({
-        
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/find/:id', async (req, res) => {
+   try {
+      const newOrder = await Order.findById(req.params.id);
+      const newUser = await User.findById(req.params.id);
+      res.status(200).json({});
+   } catch (err) {
+      res.status(500).json(err);
+   }
 });
 
-
 //GET ALL TourguideS
-router.get("/", async (req, res) => {
-
-    try{
-    
+router.get('/', async (req, res) => {
+   try {
       const Orders = await Order.find();
-       
-   
-      const Users = await User.find();
-   
-      
       res.status(200).json({
-        id:req.id,
-          Orders,
-      
-          Users,
-
+         Orders,
       });
-    }catch (err) {
+   } catch (err) {
       res.status(500).json(err);
-    }
+   }
 });
 //sadsdas
 
--- frontend_web/src/views/Tourguideview.vue
+++ frontend_web/src/views/Tourguideview.vue
@@ -1,252 +1,205 @@
 <script>
+import TourguideService from '../services/Tourguide.service';
+import toastsVue from '../components/toasts.vue';
+import { mapState } from 'pinia';
+import { useAuthStore } from '@/stores/Auth.store';
 
-    import TourguideService from "../services/Tourguide.service";
-    import toastsVue from "../components/toasts.vue";
-    import { mapState } from 'pinia'
-    import { useAuthStore } from "@/stores/Auth.store";
-   
-    import toast from "../assets/js/toasts";
-  
-        export default{
-            data(){
-                return{
-                    users:[],
-                    orders:[],
-                    activeUser:-1,
-                    activeIndexOrder:-1,
-                    toasts:{
-                        title:"Warning",
-                        msg:"Bạn không phải ADMIN",
-                        type:"warn",
-                        duration:2000
-                     },
-                      
-                        
-                }
-            
-            },
-            computed:{
-             
-    
-                getindexorder(){
-                     if(this.activeIndexOrder!=-1){
-                         const list = document.querySelectorAll(".order-item");
-                         list.forEach(element => {
-                             element.classList.remove("active");
-                         });
-                         list[this.activeIndexOrder].classList.add("active");
-                        return this.orders[this.activeIndexOrder];
-                     }
-                },
-                  getindexuser(){
-                     if(this.activeUser!=-1){
-                         const list = document.querySelectorAll(".user-item");
-                         list.forEach(element => {
-                             element.classList.remove("active");
-                         });
-                         list[this.activeUser].classList.add("active");
-                            return this.users[this.activeUser];
-                    }
-                }
-            },
-            computed:{
-            ...mapState(useAuthStore,{
-                currentUser: "user",
-            }),
-        },
-            components:{
-                  toastsVue,
-      
-             },
-            methods:{
-               
-                
-                toast,
-                
-                    async getall(){
-                try{
-                    
-                    const data = await TourguideService.getAll();
-                    this.users = data.Users;
-                    this.orders = data.Orders;
-                    
-                    console.log(this.orders.tourguideId)
-                    
+import toast from '../assets/js/toasts';
 
+export default {
+   data() {
+      return {
+         users: [],
+         orders: [],
+         activeUser: -1,
+         activeIndexOrder: -1,
+         toasts: {
+            title: 'Warning',
+            msg: 'Bạn không phải ADMIN',
+            type: 'warn',
+            duration: 2000,
+         },
+         tourguideId: '',
+      };
+   },
+   computed: {
+      getindexorder() {
+         if (this.activeIndexOrder != -1) {
+            const list = document.querySelectorAll('.order-item');
+            list.forEach((element) => {
+               element.classList.remove('active');
+            });
+            list[this.activeIndexOrder].classList.add('active');
+            return this.orders[this.activeIndexOrder];
+         }
+      },
+      getindexuser() {
+         if (this.activeUser != -1) {
+            const list = document.querySelectorAll('.user-item');
+            list.forEach((element) => {
+               element.classList.remove('active');
+            });
+            list[this.activeUser].classList.add('active');
+            return this.users[this.activeUser];
+         }
+      },
+   },
+   computed: {
+      ...mapState(useAuthStore, {
+         currentUser: 'user',
+      }),
+   },
+   components: {
+      toastsVue,
+   },
+   methods: {
+      toast,
 
-                }catch(error){
-                    this.toast();
-                        setTimeout(()=>{
-                            this.$router.push({name:"ShopMain"});
-                        },1000);
-                    }
-                },
-            },
-        created(){
-            this.getall();
-        },
-    }
-    </script>
-    
-    <template>
-       <div class="tourguide">
-        <h3>Giao diện quản lý khách hàng của hướng dẫn viên</h3>
-             <a href="">   <input type="text" placeholder="Tìm kiếm khách hàng"></a>
-       </div>
-   
-        <div class="container">
-         <div class=" row-cols-1">
-            <h4 class="heading" >Tên tour</h4>
-            <div class="title" v-for="order in orders"
-                :key="order._id"
-            >
-            
-                <span>{{order.title}}</span></div>
-               
-            </div>
-            <div class=" row-cols-1">
-            <h4 class="heading" >Tên khách</h4>
-            <div class="name" v-for="order in orders"
-                :key="order._id"
-               
-            >
-            
-                <span>{{order.name}}</span></div>
-               
-            </div>
-            <div class=" row-cols-1">
-            <h4 class="heading" >Địa chỉ</h4>
-            <div class="address" v-for="order in orders"
-                :key="order._id"
-              
-            >
-            
-                <span>{{order.address}}</span></div>
-               
-            </div>
-    
-            <div class=" row-cols-1">
-            <h4 class="heading" >Số điện thoại</h4>
-            <div class="phone" v-for="order in orders"
-                :key="order._id"
-              
-            >
-            
-                <span>{{order.phone}}</span></div>
-               
-            </div>
-        
-          
-            
-    </div>
-     </template>
-    <style scoped>
-      
-      .tourguide{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 5rem;
-      }
-     .title:hover{
-          background-color: #0d6efd;
-            color: azure;
-    
-     }
-    .col{
-        border: 1px solid  #dee2e6;
-        font-size: 14px;
-        
-    }
-    .btn button{
-       
-     
-        font-size: 18px;
-       
-         border: 1px solid #dee2e6;
-         background:#FBE2C5;
-         margin: 0 5px;
-    }
-    .btn{
-         font-weight: 500;
-        font-size: 12px;
-        padding: 20px;
-         border: 1px solid #dee2e6;
-            background: white;
-          
-    }
-    .phone{
-        height:40px;
-         font-weight: 500;
-        font-size: 14px;
-        padding: 10px;
-         border: 1px solid #dee2e6;
-        background: white;
-        width: 150px;
-          
-    }
-    .address{
-         border: 1px solid #dee2e6;
-        height: 40px;
-        font-size: 14px;
-         width: 200px; 
-        font-weight: 500;
-        padding: 10px;
-       background: white;
-        
-    
-    } 
-    .heading{
-        cursor: pointer;
-        position: relative;
-     
-       
-        font-size: 16px;
-        display: flex;
-       justify-content: center;
-       align-items: center;
-       height :35px;
-       color:#dee2e6;
-       background-color: #0d6efd;
-     
-    }
-    
-    .title{
-         border: 1px solid #dee2e6;
-        height: 40px;
-        font-size: 14px;
-         font-weight: 500;
-         padding: 10px;
-         overflow: hidden;
-        background: white;
-        
-    }
-    
-    .name{
-        
-         border: 1px solid #dee2e6;
-        height: 40px;
-        font-size: 14px;
-        width: 160px;
-         font-weight: 500;
-         padding: 10px;
-         overflow: hidden;
-        background: white;
-        
-    
-    }
-    
-    .row-cols-1{
-        /* width: 10%; */
-        height: 20%;
-       
-    
-    }
-    .container{
-        display: flex;
-        justify-content: center;
-        max-width: 100%;
-        margin-bottom: 5rem;
-    }
-    
-    </style> 
+      async getall() {
+         try {
+            const data = await TourguideService.getAll();
+            this.orders = data.Orders;
+            const user = JSON.parse(localStorage.getItem('user'));
+            this.tourguideId = user._id;
+            console.log(this.tourguideId);
+            console.log(this.orders);
+         } catch (error) {
+            this.toast();
+            setTimeout(() => {
+               this.$router.push({ name: 'ShopMain' });
+            }, 1000);
+         }
+      },
+   },
+   created() {
+      this.getall();
+   },
+};
+</script>
+
+<template>
+   <div class="tourguide">
+      <h3>Giao diện quản lý khách hàng của hướng dẫn viên</h3>
+      <a href=""> <input type="text" placeholder="Tìm kiếm khách hàng" /></a>
+   </div>
+
+   <div class="container">
+      <div class="row-cols-1">
+         <h4 class="heading">Tên tour</h4>
+         <div class="title" v-for="order in orders" :key="order._id" v-show="order.tourguideId == tourguideId">
+            <span>{{ order.title }}</span>
+         </div>
+      </div>
+      <div class="row-cols-1">
+         <h4 class="heading">Tên khách</h4>
+         <div class="name" v-for="order in orders" :key="order._id" v-show="order.tourguideId == tourguideId">
+            <span>{{ order.name }}</span>
+         </div>
+      </div>
+      <div class="row-cols-1">
+         <h4 class="heading">Địa chỉ</h4>
+         <div class="address" v-for="order in orders" :key="order._id" v-show="order.tourguideId == tourguideId">
+            <span>{{ order.address }}</span>
+         </div>
+      </div>
+
+      <div class="row-cols-1">
+         <h4 class="heading">Số điện thoại</h4>
+         <div class="phone" v-for="order in orders" :key="order._id" v-show="order.tourguideId == tourguideId">
+            <span>{{ order.phone }}</span>
+         </div>
+      </div>
+   </div>
+</template>
+<style scoped>
+.tourguide {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin-bottom: 5rem;
+}
+.title:hover {
+   background-color: #0d6efd;
+   color: azure;
+}
+.col {
+   border: 1px solid #dee2e6;
+   font-size: 14px;
+}
+.btn button {
+   font-size: 18px;
+
+   border: 1px solid #dee2e6;
+   background: #fbe2c5;
+   margin: 0 5px;
+}
+.btn {
+   font-weight: 500;
+   font-size: 12px;
+   padding: 20px;
+   border: 1px solid #dee2e6;
+   background: white;
+}
+.phone {
+   height: 40px;
+   font-weight: 500;
+   font-size: 14px;
+   padding: 10px;
+   border: 1px solid #dee2e6;
+   background: white;
+   width: 150px;
+}
+.address {
+   border: 1px solid #dee2e6;
+   height: 40px;
+   font-size: 14px;
+   width: 200px;
+   font-weight: 500;
+   padding: 10px;
+   background: white;
+}
+.heading {
+   cursor: pointer;
+   position: relative;
+
+   font-size: 16px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   height: 35px;
+   color: #dee2e6;
+   background-color: #0d6efd;
+}
+
+.title {
+   border: 1px solid #dee2e6;
+   height: 40px;
+   font-size: 14px;
+   font-weight: 500;
+   padding: 10px;
+   overflow: hidden;
+   background: white;
+}
+
+.name {
+   border: 1px solid #dee2e6;
+   height: 40px;
+   font-size: 14px;
+   width: 160px;
+   font-weight: 500;
+   padding: 10px;
+   overflow: hidden;
+   background: white;
+}
+
+.row-cols-1 {
+   /* width: 10%; */
+   height: 20%;
+}
+.container {
+   display: flex;
+   justify-content: center;
+   max-width: 100%;
+   margin-bottom: 5rem;
+}
+</style>
